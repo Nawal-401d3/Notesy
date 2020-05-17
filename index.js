cc8f86eb23a36b398faac0ecaec78afd88e30184
Nsuliman <nsuliman88@gmail.com>
2020-05-17 16:29:18 +0300

    almost done with Lab01

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-
-/**
- * / Request Handler (All Routes)
- * @param req
- * @param res
- */
-
-
-
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js')
 
