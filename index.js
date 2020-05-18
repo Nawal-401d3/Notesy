9b13d7e74da41abf408febddbd213cd4a55fb119
Nsuliman <nsuliman88@gmail.com>
2020-05-18 10:59:26 +0300

    Lab 1 Done since yasterday

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const Input = require('./lib/input.js');
-const Notes = require('./lib/notes.js')
+const Notes = require('./lib/notes.js');
 
 let parsedInput = new Input(process.argv.slice(2));
 let notes = new Notes(parsedInput);
